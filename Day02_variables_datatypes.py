6b699c2e40049a9afcc30ba7f7f89439de6375ff
Manjeet Yadav <manjeetydv258@gmail.com>
2026-02-28 20:01:46 +0530

    Create Day02_variables_datatypes.py

---
+++ Day02_variables_datatypes.py
@@ -0,0 +1,22 @@
+# Day 02 - Variables and Data Types
+# Author: Manjeet Yadav
+
+# Integer
+a = 10
+print("The type of a is", type(a))
+
+# Float
+b = 3.9
+print("The type of b is", type(b))
+
+# Boolean
+c = True
+print("The type of c is", type(c))
+
+# String
+d = "Manjeet"
+print("The type of d is", type(d))
+
+# None type
+e = None
+print("The type of e is", type(e))
